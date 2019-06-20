5ab8cd55ef3c240fed4c49e4e18809c8cb502e64
Shu OGAWARA <expajp@gmail.com>
2019-06-20 21:25:19 +0900

    implement my_forwardable

--- reimplement_forwardable/my_forwardable.rb
+++ reimplement_forwardable/my_forwardable.rb
@@ -1,9 +1,20 @@
 module MyForwardable
-  def def_deligator(accessor, method, ali = method)
-    
+  def def_delegator(accessor, method, ali = method)
+    module_eval(<<"EOS"
+      def #{ali}(*values)
+        if values.length == 0
+          #{accessor}.#{method}
+        elsif values.length == 1
+          #{accessor}.#{method}(values.first)
+        else
+          #{accessor}.#{method}(values)
+        end
+      end
+EOS
+    )
   end
 
-  def def_deligators(accessor, *methods)
-
+  def def_delegators(accessor, *methods)
+    methods.each { |method| def_delegator(accessor, method) } 
   end
 end
